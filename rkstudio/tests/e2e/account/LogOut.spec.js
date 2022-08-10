39d96013825a7e252ff8a98ba23f70ed478293c3
Olga Gnezdyonova <ognezdyonova@healthrecoverysolutions.com>
2022-08-10 12:18:31 +0300

    ✅ log out user
    Updated/Created
    LogOut.spec.js
    PO_Header.js

---
+++ rkstudio/tests/e2e/account/LogOut.spec.js
@@ -0,0 +1,50 @@
+/**
+ * - Log out
+ */
+
+import PO_Home from "../../../pages/ResearchKitStudio/PO_Home";
+import PO_Profile from "../../../pages/ResearchKitStudio/PO_Profile";
+import CR_Main from "../../../pages/ResearchKitStudio/PO_Main";
+
+describe('Log out', () => {
+    it("Log out", () => {
+        let home_page = new PO_Home();
+        home_page.header.organization_switcher_link()
+            .should("be.visible");
+
+        home_page.header.user_link()
+            .should("be.visible")
+            .realHover();
+
+        home_page.header.user_sign_out_link()
+            .should("be.visible")
+            .click({force: true});
+
+        let login = new CR_Main();
+        login.username_input()
+            .should("be.visible");
+
+        login.password_input()
+            .should("be.visible");
+
+        login.submit_button()
+            .should("be.visible");
+
+        login.forgot_password_link()
+            .should("be.visible");
+
+        cy.reload();
+
+        login.username_input()
+            .should("be.visible");
+
+        login.password_input()
+            .should("be.visible");
+
+        login.submit_button()
+            .should("be.visible");
+
+        login.forgot_password_link()
+            .should("be.visible");
+    });
+})
