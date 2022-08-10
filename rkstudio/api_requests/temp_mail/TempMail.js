d14b7648fba58164663dd1d1729fbcbec2147bbe
Olga Gnezdyonova <ognezdyonova@healthrecoverysolutions.com>
2022-08-10 11:56:05 +0300

    temp module

--- rkstudio/api_requests/temp_mail/TempMail.js
+++ rkstudio/api_requests/temp_mail/TempMail.js
@@ -1,9 +1,54 @@
-import Account from "./requests/Account";
+import Mailjs from "@cemalgnlts/mailjs";
 
 class TempMail {
     constructor() {
-        this.account = new Account();
+        this.mail = new Mailjs();
+    }
+
+    newEmail() {
+        return this.mail.createOneAccount()
+            .then(r => {
+                return r.data
+            })
+    }
+
+    login(email, password) {
+        return this.mail.login(email, password)
+            .then(r => {
+                return r.data
+            })
+    }
+
+    getMessages() {
+        return this.mail.getMessages(1)
+            .then(r => {
+                return r.data
+            })
+    }
+
+    getMessage(id) {
+        return this.mail.getMessage(id)
+            .then(r => {
+                return r.data
+            })
+    }
+
+    deleteAccount(){
+        return this.mail
+            .deleteMe()
     }
 }
 
 export default TempMail;
+
+/**
+ *         let t = new TempMail();
+ *         let a = t.newEmail().then(u => {
+ *             cy.log(u)
+ *             t.login(u.username, u.password)
+ *                 .then(console.log)
+ *             return t.getMessages();
+ *         })
+ *
+ *         a.then(console.log)
+ */
